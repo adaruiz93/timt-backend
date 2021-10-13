07107e3576fc2623521a29da4a7a3a345a150c3f
adaruiz <ada.ruiz93@gmail.com>
2021-10-12 21:14:26 -0700

    created delete route

--- controllers/timt.js
+++ controllers/timt.js
@@ -9,8 +9,8 @@ router.get('/', async (req, res) => {
   try {
     const allTimt = await Timt.find()
     res.status(200).json(allTimt)
-  } catch (error) {
-    res.status(400).json({ error: error.message})
+  } catch (err) {
+    res.status(400).json({ error: err.message})
   }
  })
  
@@ -20,6 +20,16 @@ router.post('/', async (req, res) => {
     const newTimt = await Timt.create(req.body)
     res.status(200).json(newTimt)
   } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+ })
+ 
+ // delete
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteTimt = await Timt.findByIdAndDelete(req.params.id)
+    res.status(200).json(deleteTimt)
+  } catch(err) {
     res.status(400).json({ error: err.message })
   }
  })
